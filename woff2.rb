b4584abda4bf23816c31d4dc67bae329f5c53f0d
Bram Stein <stein@adobe.com>
2014-12-18 22:57:19 +0100

    Initial woff2 formula.

---
+++ woff2.rb
@@ -0,0 +1,14 @@
+require "formula"
+
+class Sfnt2woff < Formula
+  homepage "https://github.com/google/woff2"
+  url "https://github.com/google/woff2/archive/master.tar.gz"
+  sha1 "3cc6c68bc9e933ebf768e4e15a12a24eee0ab280"
+  version "2014-12-18"
+
+  def install
+    system "make"
+    bin.install "woff2_compress"
+    bin.install "woff2_decompress"
+  end
+end
