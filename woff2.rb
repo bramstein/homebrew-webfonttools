55ed06a5ffd6d58674917ba80b8709bb9289ff8a
Bram Stein <stein@adobe.com>
2014-12-18 22:59:49 +0100

    Fix woff2 class name.

--- woff2.rb
+++ woff2.rb
@@ -1,6 +1,6 @@
 require "formula"
 
-class Sfnt2woff < Formula
+class Woff2 < Formula
   homepage "https://github.com/google/woff2"
   url "https://github.com/google/woff2/archive/master.tar.gz"
   sha1 "3cc6c68bc9e933ebf768e4e15a12a24eee0ab280"
