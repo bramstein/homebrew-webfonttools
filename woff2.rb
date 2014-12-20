bcb819f93878383195c0dd6350780c17a29d90f2
Bram Stein <stein@adobe.com>
2014-12-20 08:19:04 +0100

    Update WOFF2 formula.

--- woff2.rb
+++ woff2.rb
@@ -2,7 +2,7 @@ require "formula"
 
 class Woff2 < Formula
   homepage "https://github.com/google/woff2"
-  head "https://github.com/google/woff2.git"
+  url "https://github.com/google/woff2.git", :revision => "2b35420fa91d7999423076a516eef9b1572be5af", :using => :git
 
   def install
     system "make"
