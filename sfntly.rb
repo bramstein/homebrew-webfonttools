481eb46a3b0e6cc91d3ca0ea86e07b242aeae324
Bram Stein <stein@adobe.com>
2015-01-23 10:01:17 +0100

    Version should be a string.

--- sfntly.rb
+++ sfntly.rb
@@ -3,7 +3,7 @@ require "formula"
 class Sfntly < Formula
   homepage "https://code.google.com/p/sfntly/"
   url "http://sfntly.googlecode.com/svn/trunk/java/", :using => :svn, :revision => 239
-  version 239
+  version '239'
 
   depends_on :ant => :build
 
