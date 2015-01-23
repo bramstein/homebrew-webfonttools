0a965927d20e7f4e7554c692b007530f5c8dcb1b
Bram Stein <stein@adobe.com>
2015-01-23 09:54:17 +0100

    Use head.

--- sfntly.rb
+++ sfntly.rb
@@ -2,7 +2,7 @@ require "formula"
 
 class Sfntly < Formula
   homepage "https://code.google.com/p/sfntly/"
-  url "http://sfntly.googlecode.com/svn/trunk/java/", :revision => "r239", :using => :svn
+  head "http://sfntly.googlecode.com/svn/trunk/java/", :using => :svn
 
   depends_on :ant => :build
 
