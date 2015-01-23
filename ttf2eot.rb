d750807ee0cf463ba652ace264208d145a231882
Bram Stein <stein@adobe.com>
2015-01-23 09:27:44 +0100

    Use packaged source code.

--- ttf2eot.rb
+++ ttf2eot.rb
@@ -2,7 +2,8 @@ require "formula"
 
 class Ttf2eot < Formula
   homepage "https://code.google.com/p/ttf2eot/"
-  url "http://ttf2eot.googlecode.com/svn/trunk/", :revision => "r33", :using => :svn
+  url "https://ttf2eot.googlecode.com/files/ttf2eot-0.0.2-2.tar.gz"
+  sha1 "c9a64216e7a090cb50f7a5074865218623dea75d"
 
   def install
     system "make"
