2f1965f67d72a9be287d39ca5b2444f4aaef58f6
Bram Stein <stein@adobe.com>
2015-03-01 22:20:12 +0100

    Clean up file.

--- fonttools.rb
+++ fonttools.rb
@@ -8,10 +8,10 @@ class Fonttools < Formula
   depends_on :python if MacOS.version <= :snow_leopard
 
   def install
-    ENV["PYTHONPATH"] = libexec/"vendor/lib/python2.7/site-packages"
     ENV.prepend_create_path "PYTHONPATH", libexec/"lib/python2.7/site-packages"
 
     system "python", *Language::Python.setup_install_args(libexec)
+
     bin.install Dir["#{libexec}/bin/*"]
     bin.env_script_all_files(libexec/"bin", :PYTHONPATH => ENV["PYTHONPATH"])
   end
