f3a155d98aa1051792ae9fdc86dc0ba9e473f151
Bram Stein <stein@adobe.com>
2015-03-01 22:35:21 +0100

    Set PYTHONPATH correctly.

--- fonttools.rb
+++ fonttools.rb
@@ -8,6 +8,7 @@ class Fonttools < Formula
   depends_on :python if MacOS.version <= :snow_leopard
 
   def install
+    ENV["PYTHONPATH"] = libexec/"lib/python2.7/site-packages"
     ENV.prepend_create_path "PYTHONPATH", libexec/"lib/python2.7/site-packages"
 
     system "python", *Language::Python.setup_install_args(libexec)
