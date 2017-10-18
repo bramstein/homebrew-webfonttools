39a5515ea9dde593988e78224049b6eb66ad6eba
Masaya Nakamura <mnakamura@typeproject.com>
2017-10-18 12:21:52 +0900

    Add optional FreeType dependency for ots-side-by-side

--- ots.rb
+++ ots.rb
@@ -10,16 +10,22 @@ class Ots < Formula
   depends_on "automake"   => :build
   depends_on "libtool"    => :build
   depends_on "pkg-config" => :build
+  depends_on "freetype"   => :optional
 
   def install
+    if build.with? "freetype"
+      ENV.prepend_path "PKG_CONFIG_PATH", Formula["freetype"].opt_lib/"pkgconfig"
+    end
+
     system "./autogen.sh" if build.head?
     system "./configure"
     system "make", "CXXFLAGS=-DOTS_DEBUG"
+
     bin.install "ots-sanitize"
     bin.install "ots-perf"
-    bin.install "ots-side-by-side"
     bin.install "ots-idempotent"
     bin.install "ots-validator-checker"
+    bin.install "ots-side-by-side" if build.with? "freetype"
   end
 
   test do
